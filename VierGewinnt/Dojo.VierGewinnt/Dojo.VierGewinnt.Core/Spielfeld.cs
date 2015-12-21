2d77b5f82725cdd5aa12c8484c071af8a1e104ad
jafz <karotte@gmail.com>
2015-12-21 21:54:40 +0100

    erstes spielfeld, krude Logik. 2 unit tests, die schief gehen

---
+++ VierGewinnt/Dojo.VierGewinnt/Dojo.VierGewinnt.Core/Spielfeld.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo.VierGewinnt.Core
+{
+    public class Spielfeld
+    {
+        private Spieler[,] _alleFelder;
+
+        public Spielfeld()
+        {
+            _alleFelder = new Spieler[7, 7];
+        }
+
+        public Spieler FeldIstBelegt(int row, int col)
+        {
+            return _alleFelder[row,col];
+        }
+
+        public void SetzeSpalte(Spieler spieler1, int i)
+        {
+            throw new NotImplementedException();
+
+            // hier sollte nun in spalte i, ein stein so gesetzt werden, dass er oben drauf fällt...
+        }
+    }
+
+    public enum Spieler
+    {
+        Niemand,
+        Spieler1,
+        Spieler2,
+    }
+}
---
+++ VierGewinnt/Dojo.VierGewinnt/Dojo.VierGewinnt.Tests/SpielFeldTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dojo.VierGewinnt.Core;
+using NUnit.Framework;
+
+namespace Dojo.VierGewinnt.Tests
+{
+    [TestFixture]
+    public class SpielFeldTests
+    {
+        [Test]
+        public void FeldBelegtVon_ValideKoordinaten_LeeresSpielfeld()
+        {
+            var spielFeld = new Spielfeld();
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Assert.That(spielFeld.FeldIstBelegt(i, j), Is.EqualTo(Spieler.Niemand));
+                }
+            }
+        }
+
+        [Test]
+        public void FieldBelegtVon_KoordinatenAußerhalbDesSpielfeldes_AttributeException()
+        {
+            var spielFeld = new Spielfeld();
+
+            Assert.That(spielFeld.FeldIstBelegt(8,7), Throws.ArgumentException);
+            Assert.That(spielFeld.FeldIstBelegt(7,8), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void SetzeSpalte_Spieler1Setzt_BelegtVonSpieler1()
+        {
+            var spielFeld = new Spielfeld();
+
+            spielFeld.SetzeSpalte(Spieler.Spieler1, 3);
+            Assert.That(spielFeld.FeldIstBelegt(0,3), Is.EqualTo(Spieler.Spieler1));
+            Assert.That(spielFeld.FeldIstBelegt(1,3), Is.EqualTo(Spieler.Niemand));
+        }
+
+        [Test]
+        public void SetzeSpalte_Spieler1Setzt2xSelbeSpalte_2FelderÜbereinanderBelegt()
+        {
+            var spielFeld = new Spielfeld();
+
+            spielFeld.SetzeSpalte(Spieler.Spieler1, 3);
+            spielFeld.SetzeSpalte(Spieler.Spieler1, 3);
+            Assert.That(spielFeld.FeldIstBelegt(1, 3), Is.EqualTo(Spieler.Spieler1));
+        }
+
+
+    }
+}
